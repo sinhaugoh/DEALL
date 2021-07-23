e6060e002c2fac50df429485977444787da84f09
Goh Sin Hau <shgoh025@mymail.sim.edu.sg>
2021-07-24 01:25:40 +0800

    create placeholder page (welcome page and splash page)

---
+++ lib/splash/splash_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //TODO: implement splash page
+    return const Scaffold(
+      body: Center(child: Text('this is splash page!')),
+    );
+  }
+}
