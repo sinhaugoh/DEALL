24fdd079803ad67f6043fbe1ef38d1c63e5629cf
Goh Sin Hau <shgoh025@mymail.sim.edu.sg>
2021-07-23 16:37:19 +0800

    restructure main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+import 'core/presentation/app_widget.dart';
+
+Future<void> main() async {
+  runApp(AppWidget());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MaterialApp(),
-    );
-  }
-}
