16e4bf14d1d0c293720c64948cfdfa66f7e6a8f8
Goh Sin Hau <shgoh025@mymail.sim.edu.sg>
2021-07-24 17:50:03 +0800

    implement value failures union class

---
+++ lib/core/application/value_failures.dart
@@ -0,0 +1,11 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'value_failures.freezed.dart';
+
+@freezed
+class ValueFailure with _$ValueFailure {
+  const ValueFailure._();
+  const factory ValueFailure.empty() = Empty;
+  const factory ValueFailure.invalidEmail() = InvalidEmail;
+  const factory ValueFailure.invalidPassword() = InvalidPassword;
+}
