a9b6444904adcc50d460a3759562b51a6b5cc70e
Goh Sin Hau <shgoh025@mymail.sim.edu.sg>
2021-07-24 23:27:04 +0800

    create empty retailer home page

--- lib/core/presentation/routes/app_router.gr.dart
+++ lib/core/presentation/routes/app_router.gr.dart
@@ -7,9 +7,10 @@
 import 'package:auto_route/auto_route.dart' as _i1;
 import 'package:flutter/material.dart' as _i2;
 
-import '../../../auth/presentation/consumer_home_page.dart' as _i5;
-import '../../../auth/presentation/sign_in_page.dart' as _i6;
+import '../../../auth/presentation/sign_in_page.dart' as _i7;
 import '../../../auth/presentation/welcome_page.dart' as _i4;
+import '../../../consumer/home/presentation/consumer_home_page.dart' as _i5;
+import '../../../retailer/home/presentation/retailer_home_page.dart' as _i6;
 import '../../../splash/splash_page.dart' as _i3;
 
 class AppRouter extends _i1.RootStackRouter {
@@ -33,10 +34,15 @@ class AppRouter extends _i1.RootStackRouter {
         builder: (_) {
           return const _i5.ConsumerHomePage();
         }),
+    RetailerHomeRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
+        routeData: routeData,
+        builder: (_) {
+          return const _i6.RetailerHomePage();
+        }),
     SignInRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
         routeData: routeData,
         builder: (_) {
-          return const _i6.SignInPage();
+          return const _i7.SignInPage();
         })
   };
 
@@ -45,6 +51,7 @@ class AppRouter extends _i1.RootStackRouter {
         _i1.RouteConfig(SplashRoute.name, path: '/'),
         _i1.RouteConfig(WelcomeRoute.name, path: '/welcome-page'),
         _i1.RouteConfig(ConsumerHomeRoute.name, path: '/consumer-home-page'),
+        _i1.RouteConfig(RetailerHomeRoute.name, path: '/retailer-home-page'),
         _i1.RouteConfig(SignInRoute.name, path: '/sign-in-page')
       ];
 }
@@ -65,6 +72,12 @@ class ConsumerHomeRoute extends _i1.PageRouteInfo {
   const ConsumerHomeRoute() : super(name, path: '/consumer-home-page');
 
   static const String name = 'ConsumerHomeRoute';
+}
+
+class RetailerHomeRoute extends _i1.PageRouteInfo {
+  const RetailerHomeRoute() : super(name, path: '/retailer-home-page');
+
+  static const String name = 'RetailerHomeRoute';
 }
 
 class SignInRoute extends _i1.PageRouteInfo {
