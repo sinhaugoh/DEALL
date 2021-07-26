c13e7b7d4d467f921c2fd9ec9f7013bb65b7bb85
Kwang Kian Hui <2k1h297@gmail.com>
2021-07-26 22:38:41 +0800

    Primary Swatch
    primary material color

--- lib/core/presentation/app_widget.dart
+++ lib/core/presentation/app_widget.dart
@@ -56,10 +56,24 @@ class AppWidget extends ConsumerWidget {
     return MaterialApp.router(
       title: 'DEALL',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: MaterialColor(0xFFF86B6B, deallAppColor),
       ),
       routeInformationParser: appRouter.defaultRouteParser(),
       routerDelegate: appRouter.delegate(),
     );
   }
 }
+
+Map<int, Color> deallAppColor =
+const {
+50: Color.fromRGBO(248, 107, 107, 0.1),
+100: Color.fromRGBO(248, 107, 107, 0.2),
+200: Color.fromRGBO(248, 107, 107, 0.3),
+300: Color.fromRGBO(248, 107, 107, 0.4),
+400: Color.fromRGBO(248, 107, 107, 0.5),
+500: Color.fromRGBO(248, 107, 107, 0.6),
+600: Color.fromRGBO(248, 107, 107, 0.7),
+700: Color.fromRGBO(248, 107, 107, 0.8),
+800: Color.fromRGBO(248, 107, 107, 0.9),
+900: Color.fromRGBO(248, 107, 107, 1.0),
+};
