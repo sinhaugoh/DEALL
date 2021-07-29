b774888c50021e7f004147c6603bcf3c45266315
Goh Sin Hau <shgoh025@mymail.sim.edu.sg>
2021-07-29 13:24:17 +0800

    implement sign out logic in presentation layer

--- lib/core/presentation/widgets/drawer_widget.dart
+++ lib/core/presentation/widgets/drawer_widget.dart
@@ -1,10 +1,12 @@
+import 'package:deall/auth/shared/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ConsumerDrawer extends StatelessWidget {
+class ConsumerDrawer extends ConsumerWidget {
   const ConsumerDrawer({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Drawer(
         child: Column(
       children: [
@@ -21,7 +23,9 @@ class ConsumerDrawer extends StatelessWidget {
         ),
         ListTile(
           leading: const Text("Sign Out"),
-          onTap: () {},
+          onTap: () {
+            ref.read(authNotifierProvider.notifier).signOut();
+          },
         ),
       ],
     ));
