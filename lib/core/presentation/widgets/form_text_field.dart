d49eafe6ca2909ece7f20f782940d319746f9dd2
Goh Sin Hau <shgoh025@mymail.sim.edu.sg>
2021-07-31 23:43:29 +0800

    add in more parameter for FormTextField

--- lib/core/presentation/widgets/form_text_field.dart
+++ lib/core/presentation/widgets/form_text_field.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class FormTextField extends StatelessWidget {
   final String label;
   final String? errorText;
+  final bool obscureText;
+  final TextInputType? keyboardType;
+  final int? maxLength;
+  final List<TextInputFormatter>? inputFormatters;
   final void Function(String value) onChanged;
+
   const FormTextField({
     Key? key,
-    required this.errorText,
+    this.errorText,
+    this.keyboardType,
+    this.obscureText = false,
+    this.inputFormatters,
+    this.maxLength,
     required this.onChanged,
     required this.label,
   }) : super(key: key);
@@ -14,12 +24,16 @@ class FormTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      maxLength: maxLength,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
       decoration: InputDecoration(
         labelText: label,
         border: const OutlineInputBorder(),
         errorText: errorText,
       ),
       onChanged: onChanged,
+      inputFormatters: inputFormatters,
     );
   }
 }
