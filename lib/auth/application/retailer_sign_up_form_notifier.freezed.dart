24a2dea6f55b1a70df169d082613b45aa083ea38
Goh Sin Hau <shgoh025@mymail.sim.edu.sg>
2021-07-31 23:46:38 +0800

    Create retailer_sign_up_form_notifier.freezed.dart

---
+++ lib/auth/application/retailer_sign_up_form_notifier.freezed.dart
@@ -0,0 +1,463 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'retailer_sign_up_form_notifier.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$RetailerSignUpFormStateTearOff {
+  const _$RetailerSignUpFormStateTearOff();
+
+  _RetailerSignUpFormState call(
+      {required bool hasConnection,
+      required bool showErrorMessage,
+      required bool isSaving,
+      required bool successful,
+      required String? emailErrorMessage,
+      required String? passwordErrorMessage,
+      required String? nameErrorMessage,
+      required String? postalCodeErrorMessage,
+      required String? uenErrorMessage,
+      required String email,
+      required String password,
+      required Retailer retailer,
+      required File? image}) {
+    return _RetailerSignUpFormState(
+      hasConnection: hasConnection,
+      showErrorMessage: showErrorMessage,
+      isSaving: isSaving,
+      successful: successful,
+      emailErrorMessage: emailErrorMessage,
+      passwordErrorMessage: passwordErrorMessage,
+      nameErrorMessage: nameErrorMessage,
+      postalCodeErrorMessage: postalCodeErrorMessage,
+      uenErrorMessage: uenErrorMessage,
+      email: email,
+      password: password,
+      retailer: retailer,
+      image: image,
+    );
+  }
+}
+
+/// @nodoc
+const $RetailerSignUpFormState = _$RetailerSignUpFormStateTearOff();
+
+/// @nodoc
+mixin _$RetailerSignUpFormState {
+  bool get hasConnection => throw _privateConstructorUsedError;
+  bool get showErrorMessage => throw _privateConstructorUsedError;
+  bool get isSaving => throw _privateConstructorUsedError;
+  bool get successful => throw _privateConstructorUsedError;
+  String? get emailErrorMessage => throw _privateConstructorUsedError;
+  String? get passwordErrorMessage => throw _privateConstructorUsedError;
+  String? get nameErrorMessage => throw _privateConstructorUsedError;
+  String? get postalCodeErrorMessage => throw _privateConstructorUsedError;
+  String? get uenErrorMessage => throw _privateConstructorUsedError;
+  String get email => throw _privateConstructorUsedError;
+  String get password => throw _privateConstructorUsedError;
+  Retailer get retailer => throw _privateConstructorUsedError;
+  File? get image => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $RetailerSignUpFormStateCopyWith<RetailerSignUpFormState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $RetailerSignUpFormStateCopyWith<$Res> {
+  factory $RetailerSignUpFormStateCopyWith(RetailerSignUpFormState value,
+          $Res Function(RetailerSignUpFormState) then) =
+      _$RetailerSignUpFormStateCopyWithImpl<$Res>;
+  $Res call(
+      {bool hasConnection,
+      bool showErrorMessage,
+      bool isSaving,
+      bool successful,
+      String? emailErrorMessage,
+      String? passwordErrorMessage,
+      String? nameErrorMessage,
+      String? postalCodeErrorMessage,
+      String? uenErrorMessage,
+      String email,
+      String password,
+      Retailer retailer,
+      File? image});
+
+  $RetailerCopyWith<$Res> get retailer;
+}
+
+/// @nodoc
+class _$RetailerSignUpFormStateCopyWithImpl<$Res>
+    implements $RetailerSignUpFormStateCopyWith<$Res> {
+  _$RetailerSignUpFormStateCopyWithImpl(this._value, this._then);
+
+  final RetailerSignUpFormState _value;
+  // ignore: unused_field
+  final $Res Function(RetailerSignUpFormState) _then;
+
+  @override
+  $Res call({
+    Object? hasConnection = freezed,
+    Object? showErrorMessage = freezed,
+    Object? isSaving = freezed,
+    Object? successful = freezed,
+    Object? emailErrorMessage = freezed,
+    Object? passwordErrorMessage = freezed,
+    Object? nameErrorMessage = freezed,
+    Object? postalCodeErrorMessage = freezed,
+    Object? uenErrorMessage = freezed,
+    Object? email = freezed,
+    Object? password = freezed,
+    Object? retailer = freezed,
+    Object? image = freezed,
+  }) {
+    return _then(_value.copyWith(
+      hasConnection: hasConnection == freezed
+          ? _value.hasConnection
+          : hasConnection // ignore: cast_nullable_to_non_nullable
+              as bool,
+      showErrorMessage: showErrorMessage == freezed
+          ? _value.showErrorMessage
+          : showErrorMessage // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isSaving: isSaving == freezed
+          ? _value.isSaving
+          : isSaving // ignore: cast_nullable_to_non_nullable
+              as bool,
+      successful: successful == freezed
+          ? _value.successful
+          : successful // ignore: cast_nullable_to_non_nullable
+              as bool,
+      emailErrorMessage: emailErrorMessage == freezed
+          ? _value.emailErrorMessage
+          : emailErrorMessage // ignore: cast_nullable_to_non_nullable
+              as String?,
+      passwordErrorMessage: passwordErrorMessage == freezed
+          ? _value.passwordErrorMessage
+          : passwordErrorMessage // ignore: cast_nullable_to_non_nullable
+              as String?,
+      nameErrorMessage: nameErrorMessage == freezed
+          ? _value.nameErrorMessage
+          : nameErrorMessage // ignore: cast_nullable_to_non_nullable
+              as String?,
+      postalCodeErrorMessage: postalCodeErrorMessage == freezed
+          ? _value.postalCodeErrorMessage
+          : postalCodeErrorMessage // ignore: cast_nullable_to_non_nullable
+              as String?,
+      uenErrorMessage: uenErrorMessage == freezed
+          ? _value.uenErrorMessage
+          : uenErrorMessage // ignore: cast_nullable_to_non_nullable
+              as String?,
+      email: email == freezed
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String,
+      password: password == freezed
+          ? _value.password
+          : password // ignore: cast_nullable_to_non_nullable
+              as String,
+      retailer: retailer == freezed
+          ? _value.retailer
+          : retailer // ignore: cast_nullable_to_non_nullable
+              as Retailer,
+      image: image == freezed
+          ? _value.image
+          : image // ignore: cast_nullable_to_non_nullable
+              as File?,
+    ));
+  }
+
+  @override
+  $RetailerCopyWith<$Res> get retailer {
+    return $RetailerCopyWith<$Res>(_value.retailer, (value) {
+      return _then(_value.copyWith(retailer: value));
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$RetailerSignUpFormStateCopyWith<$Res>
+    implements $RetailerSignUpFormStateCopyWith<$Res> {
+  factory _$RetailerSignUpFormStateCopyWith(_RetailerSignUpFormState value,
+          $Res Function(_RetailerSignUpFormState) then) =
+      __$RetailerSignUpFormStateCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {bool hasConnection,
+      bool showErrorMessage,
+      bool isSaving,
+      bool successful,
+      String? emailErrorMessage,
+      String? passwordErrorMessage,
+      String? nameErrorMessage,
+      String? postalCodeErrorMessage,
+      String? uenErrorMessage,
+      String email,
+      String password,
+      Retailer retailer,
+      File? image});
+
+  @override
+  $RetailerCopyWith<$Res> get retailer;
+}
+
+/// @nodoc
+class __$RetailerSignUpFormStateCopyWithImpl<$Res>
+    extends _$RetailerSignUpFormStateCopyWithImpl<$Res>
+    implements _$RetailerSignUpFormStateCopyWith<$Res> {
+  __$RetailerSignUpFormStateCopyWithImpl(_RetailerSignUpFormState _value,
+      $Res Function(_RetailerSignUpFormState) _then)
+      : super(_value, (v) => _then(v as _RetailerSignUpFormState));
+
+  @override
+  _RetailerSignUpFormState get _value =>
+      super._value as _RetailerSignUpFormState;
+
+  @override
+  $Res call({
+    Object? hasConnection = freezed,
+    Object? showErrorMessage = freezed,
+    Object? isSaving = freezed,
+    Object? successful = freezed,
+    Object? emailErrorMessage = freezed,
+    Object? passwordErrorMessage = freezed,
+    Object? nameErrorMessage = freezed,
+    Object? postalCodeErrorMessage = freezed,
+    Object? uenErrorMessage = freezed,
+    Object? email = freezed,
+    Object? password = freezed,
+    Object? retailer = freezed,
+    Object? image = freezed,
+  }) {
+    return _then(_RetailerSignUpFormState(
+      hasConnection: hasConnection == freezed
+          ? _value.hasConnection
+          : hasConnection // ignore: cast_nullable_to_non_nullable
+              as bool,
+      showErrorMessage: showErrorMessage == freezed
+          ? _value.showErrorMessage
+          : showErrorMessage // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isSaving: isSaving == freezed
+          ? _value.isSaving
+          : isSaving // ignore: cast_nullable_to_non_nullable
+              as bool,
+      successful: successful == freezed
+          ? _value.successful
+          : successful // ignore: cast_nullable_to_non_nullable
+              as bool,
+      emailErrorMessage: emailErrorMessage == freezed
+          ? _value.emailErrorMessage
+          : emailErrorMessage // ignore: cast_nullable_to_non_nullable
+              as String?,
+      passwordErrorMessage: passwordErrorMessage == freezed
+          ? _value.passwordErrorMessage
+          : passwordErrorMessage // ignore: cast_nullable_to_non_nullable
+              as String?,
+      nameErrorMessage: nameErrorMessage == freezed
+          ? _value.nameErrorMessage
+          : nameErrorMessage // ignore: cast_nullable_to_non_nullable
+              as String?,
+      postalCodeErrorMessage: postalCodeErrorMessage == freezed
+          ? _value.postalCodeErrorMessage
+          : postalCodeErrorMessage // ignore: cast_nullable_to_non_nullable
+              as String?,
+      uenErrorMessage: uenErrorMessage == freezed
+          ? _value.uenErrorMessage
+          : uenErrorMessage // ignore: cast_nullable_to_non_nullable
+              as String?,
+      email: email == freezed
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String,
+      password: password == freezed
+          ? _value.password
+          : password // ignore: cast_nullable_to_non_nullable
+              as String,
+      retailer: retailer == freezed
+          ? _value.retailer
+          : retailer // ignore: cast_nullable_to_non_nullable
+              as Retailer,
+      image: image == freezed
+          ? _value.image
+          : image // ignore: cast_nullable_to_non_nullable
+              as File?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_RetailerSignUpFormState extends _RetailerSignUpFormState {
+  const _$_RetailerSignUpFormState(
+      {required this.hasConnection,
+      required this.showErrorMessage,
+      required this.isSaving,
+      required this.successful,
+      required this.emailErrorMessage,
+      required this.passwordErrorMessage,
+      required this.nameErrorMessage,
+      required this.postalCodeErrorMessage,
+      required this.uenErrorMessage,
+      required this.email,
+      required this.password,
+      required this.retailer,
+      required this.image})
+      : super._();
+
+  @override
+  final bool hasConnection;
+  @override
+  final bool showErrorMessage;
+  @override
+  final bool isSaving;
+  @override
+  final bool successful;
+  @override
+  final String? emailErrorMessage;
+  @override
+  final String? passwordErrorMessage;
+  @override
+  final String? nameErrorMessage;
+  @override
+  final String? postalCodeErrorMessage;
+  @override
+  final String? uenErrorMessage;
+  @override
+  final String email;
+  @override
+  final String password;
+  @override
+  final Retailer retailer;
+  @override
+  final File? image;
+
+  @override
+  String toString() {
+    return 'RetailerSignUpFormState(hasConnection: $hasConnection, showErrorMessage: $showErrorMessage, isSaving: $isSaving, successful: $successful, emailErrorMessage: $emailErrorMessage, passwordErrorMessage: $passwordErrorMessage, nameErrorMessage: $nameErrorMessage, postalCodeErrorMessage: $postalCodeErrorMessage, uenErrorMessage: $uenErrorMessage, email: $email, password: $password, retailer: $retailer, image: $image)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _RetailerSignUpFormState &&
+            (identical(other.hasConnection, hasConnection) ||
+                const DeepCollectionEquality()
+                    .equals(other.hasConnection, hasConnection)) &&
+            (identical(other.showErrorMessage, showErrorMessage) ||
+                const DeepCollectionEquality()
+                    .equals(other.showErrorMessage, showErrorMessage)) &&
+            (identical(other.isSaving, isSaving) ||
+                const DeepCollectionEquality()
+                    .equals(other.isSaving, isSaving)) &&
+            (identical(other.successful, successful) ||
+                const DeepCollectionEquality()
+                    .equals(other.successful, successful)) &&
+            (identical(other.emailErrorMessage, emailErrorMessage) ||
+                const DeepCollectionEquality()
+                    .equals(other.emailErrorMessage, emailErrorMessage)) &&
+            (identical(other.passwordErrorMessage, passwordErrorMessage) ||
+                const DeepCollectionEquality().equals(
+                    other.passwordErrorMessage, passwordErrorMessage)) &&
+            (identical(other.nameErrorMessage, nameErrorMessage) ||
+                const DeepCollectionEquality()
+                    .equals(other.nameErrorMessage, nameErrorMessage)) &&
+            (identical(other.postalCodeErrorMessage, postalCodeErrorMessage) ||
+                const DeepCollectionEquality().equals(
+                    other.postalCodeErrorMessage, postalCodeErrorMessage)) &&
+            (identical(other.uenErrorMessage, uenErrorMessage) ||
+                const DeepCollectionEquality()
+                    .equals(other.uenErrorMessage, uenErrorMessage)) &&
+            (identical(other.email, email) ||
+                const DeepCollectionEquality().equals(other.email, email)) &&
+            (identical(other.password, password) ||
+                const DeepCollectionEquality()
+                    .equals(other.password, password)) &&
+            (identical(other.retailer, retailer) ||
+                const DeepCollectionEquality()
+                    .equals(other.retailer, retailer)) &&
+            (identical(other.image, image) ||
+                const DeepCollectionEquality().equals(other.image, image)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(hasConnection) ^
+      const DeepCollectionEquality().hash(showErrorMessage) ^
+      const DeepCollectionEquality().hash(isSaving) ^
+      const DeepCollectionEquality().hash(successful) ^
+      const DeepCollectionEquality().hash(emailErrorMessage) ^
+      const DeepCollectionEquality().hash(passwordErrorMessage) ^
+      const DeepCollectionEquality().hash(nameErrorMessage) ^
+      const DeepCollectionEquality().hash(postalCodeErrorMessage) ^
+      const DeepCollectionEquality().hash(uenErrorMessage) ^
+      const DeepCollectionEquality().hash(email) ^
+      const DeepCollectionEquality().hash(password) ^
+      const DeepCollectionEquality().hash(retailer) ^
+      const DeepCollectionEquality().hash(image);
+
+  @JsonKey(ignore: true)
+  @override
+  _$RetailerSignUpFormStateCopyWith<_RetailerSignUpFormState> get copyWith =>
+      __$RetailerSignUpFormStateCopyWithImpl<_RetailerSignUpFormState>(
+          this, _$identity);
+}
+
+abstract class _RetailerSignUpFormState extends RetailerSignUpFormState {
+  const factory _RetailerSignUpFormState(
+      {required bool hasConnection,
+      required bool showErrorMessage,
+      required bool isSaving,
+      required bool successful,
+      required String? emailErrorMessage,
+      required String? passwordErrorMessage,
+      required String? nameErrorMessage,
+      required String? postalCodeErrorMessage,
+      required String? uenErrorMessage,
+      required String email,
+      required String password,
+      required Retailer retailer,
+      required File? image}) = _$_RetailerSignUpFormState;
+  const _RetailerSignUpFormState._() : super._();
+
+  @override
+  bool get hasConnection => throw _privateConstructorUsedError;
+  @override
+  bool get showErrorMessage => throw _privateConstructorUsedError;
+  @override
+  bool get isSaving => throw _privateConstructorUsedError;
+  @override
+  bool get successful => throw _privateConstructorUsedError;
+  @override
+  String? get emailErrorMessage => throw _privateConstructorUsedError;
+  @override
+  String? get passwordErrorMessage => throw _privateConstructorUsedError;
+  @override
+  String? get nameErrorMessage => throw _privateConstructorUsedError;
+  @override
+  String? get postalCodeErrorMessage => throw _privateConstructorUsedError;
+  @override
+  String? get uenErrorMessage => throw _privateConstructorUsedError;
+  @override
+  String get email => throw _privateConstructorUsedError;
+  @override
+  String get password => throw _privateConstructorUsedError;
+  @override
+  Retailer get retailer => throw _privateConstructorUsedError;
+  @override
+  File? get image => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$RetailerSignUpFormStateCopyWith<_RetailerSignUpFormState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
