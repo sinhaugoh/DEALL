7b1d2ab6576000915fc40cbf47b98b11027a2e57
Goh Sin Hau <shgoh025@mymail.sim.edu.sg>
2021-08-01 23:03:06 +0800

    implement routing logic for retailer sign up feature

--- lib/auth/presentation/retailer_sign_up_page.dart
+++ lib/auth/presentation/retailer_sign_up_page.dart
@@ -1,5 +1,8 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:deall/auth/application/retailer_sign_up_form_notifier.dart';
 import 'package:deall/auth/presentation/widget/retailer_sign_up_form.dart';
 import 'package:deall/auth/shared/providers.dart';
+import 'package:deall/core/presentation/routes/app_router.gr.dart';
 import 'package:deall/core/presentation/saving_in_progress_overlay.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -9,6 +12,30 @@ class RetailerSignUpPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+        ref.listen<RetailerSignUpFormState>(retailerSignUpFormNotifierProvider,
+        (state) {
+      if (state.successful) {
+        AutoRouter.of(context).pushAndPopUntil(
+          const RetailerHomeRoute(),
+          predicate: (_) => false,
+        );
+      }
+    });
+
+    ref.listen<bool>(
+      retailerSignUpFormNotifierProvider.select((state) => state.hasConnection),
+      (state) {
+        if (state == false) {
+          //TODO: use theme snackbar instead
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text('No connection'),
+            duration: Duration(seconds: 5),
+            behavior: SnackBarBehavior.floating,
+          ));
+        }
+      },
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sign Up'),
