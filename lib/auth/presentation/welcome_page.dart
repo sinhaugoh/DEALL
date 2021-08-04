d49d1816fb246fa2f0f3145ced6cf27bcfca18ae
Lawrence Ho <lawrencehsj@gmail.com>
2021-08-04 12:32:07 +0800

    added dividers to separate buttons and text

--- lib/auth/presentation/welcome_page.dart
+++ lib/auth/presentation/welcome_page.dart
@@ -1,9 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:deall/auth/presentation/widget/sign_in_form.dart';
 import 'package:deall/core/presentation/const/size_config.dart';
+import 'package:deall/core/presentation/const/themedata_swatch_color.dart';
 import 'package:deall/core/presentation/routes/app_router.gr.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WelcomePage extends ConsumerWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -13,24 +15,19 @@ class WelcomePage extends ConsumerWidget {
     return Scaffold(
       body: Center(
         child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Expanded(
-              flex: 4,
-              child: Align(
-                // alignment: Alignment.bottomLeft,
-                child: WelcomeWidget()
-              )
+              flex: 2,
+              child: WelcomeWidget()
             ),
-            Expanded(
-              flex: 3,
-              child: SignInForm(),
-            ),
+            // Expanded(
+            //   flex: 3,
+            //   child: SignInForm(),
+            // ),
             Expanded(
               flex: 2,
               child: ButtonWidget()
             ),
-            
           ],
         ),
       ),
@@ -42,41 +39,32 @@ class WelcomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      // height: SizeConfig.heightMultiplier*35,
-      // width: MediaQuery.of(context).size.width,
-      // color: Colors.blue,
-      // alignment: Alignment.bottomLeft,
       child: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        // mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
             flex: 4,
             child: Align(
-              // color: Colors.blue,
-              alignment: Alignment.bottomLeft,
+              alignment: Alignment.bottomCenter,
               child: Text(
                 'Welcome.',
-                style: Theme.of(context).textTheme.headline2,
-                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 8.0 * SizeConfig.textMultiplier,
+                  fontWeight: FontWeight.w500
+                ),
+                textAlign: TextAlign.center,
               ),
             ),
           ),
-          // Text(
-          //   'Late night deals for your supper needs.',
-          //   style: Theme.of(context).textTheme.headline3,
-          //   textAlign: TextAlign.left,
-          // )
-          Expanded(
-            flex:1,
-            child: Container(
-              color: Colors.grey,
-              child: Text(
-                'test',
-                style: Theme.of(context).textTheme.headline3,
-              )
-            ),
+          Align(
+            child: Text(
+              'Late night deals for your supper needs.',
+              style: GoogleFonts.archivo(
+                textStyle: Theme.of(context).textTheme.subtitle2,
+              ),
+              textAlign: TextAlign.center,
+            )
           ),
         ],
       ),
@@ -95,7 +83,7 @@ class SignInForm extends StatelessWidget {
           Expanded(
             // flex:2,
             child: Text('test'),
-            // SignInForm()
+            // child: SignInForm()
           )
         ],
       )
@@ -103,56 +91,59 @@ class SignInForm extends StatelessWidget {
   }
 }
 
-//Container
-      // height: SizeConfig.heightMultiplier*35,
-      // width: MediaQuery.of(context).size.width,
-      // color: Colors.blue,
-      // alignment: Alignment.bottomLeft,
-
 class ButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.yellow,
+      // color: Colors.yellow,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 18.0),
-            child: ElevatedButton(
-              onPressed: () {
-                AutoRouter.of(context).push(const SignInRoute());
-              },
-              child: const Text('sign in'),
-              style: ButtonStyle(
-                  shape: MaterialStateProperty.all(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25)
-                    )
-                 )
-               ),
-              ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 18.0),
-            child: ElevatedButton(
-              onPressed: () {
-                AutoRouter.of(context).push(const SignUpRoute());
-              },
-              child: const Text('sign up'),
-              style: ButtonStyle(
-                  shape: MaterialStateProperty.all(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25)
-                    )
+            padding: EdgeInsets.symmetric(horizontal: 8.0 * SizeConfig.heightMultiplier, vertical: 1.0*SizeConfig.heightMultiplier),
+            child: ConstrainedBox(
+              constraints: BoxConstraints.tightFor(height: 7*SizeConfig.heightMultiplier),
+              child: ElevatedButton(
+                onPressed: () {
+                  AutoRouter.of(context).push(const SignInRoute());
+                },
+                child: const Text('Sign-in'),
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30))
+                   )
                  ),
-               ),
-               
+                ),
             ),
           ),
 
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.0 * SizeConfig.heightMultiplier, vertical: 1.0*SizeConfig.heightMultiplier),
+            child: ConstrainedBox(
+              constraints: BoxConstraints.tightFor(height: 7*SizeConfig.heightMultiplier),
+              child: ElevatedButton(
+                onPressed: () {
+                  AutoRouter.of(context).push(const SignUpRoute());
+                },
+                child: const Text('Sign-up',style: TextStyle(color: Colors.black)),
+                style: ButtonStyle(
+                    side: MaterialStateProperty.all(BorderSide(color: Color(0xFFF86B6B))),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)
+                      )
+                    ),
+                    backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFFFF7EC)),
+                 ),
+              ),
+            ),
+          ),
+        Divider(
+          // thickness: 2,
+          // indent: 4.5 * SizeConfig.heightMultiplier,
+          // endIndent: 4.5 * SizeConfig.heightMultiplier,
+          height: 18 * SizeConfig.heightMultiplier,
+        )
         ],
       ),
     );
--- lib/core/presentation/const/style_theme.dart
+++ lib/core/presentation/const/style_theme.dart
@@ -1,6 +1,7 @@
 import 'package:deall/core/presentation/const/themedata_swatch_color.dart';
 import 'package:flutter/material.dart';
 import 'package:deall/core/presentation/const/size_config.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StyleTheme{
   StyleTheme._();
@@ -24,19 +25,22 @@ class StyleTheme{
     button: _buttonLight,
     headline4: _greetingLight,
     headline3: _searchLight,
-    bodyText2: _selectedTabLight,
-    bodyText1: _unSelectedTabLight,  
+    bodyText1: _selectedTabLight,
+    bodyText2: _unSelectedTabLight,  
   );
 
   static final TextStyle _titleLight = TextStyle(
     color: Colors.black,
-    fontSize: 3.5 * SizeConfig.textMultiplier,
+    fontSize: 8.0 * SizeConfig.textMultiplier,
+    fontWeight: FontWeight.w500
   );
 
   static final TextStyle _subTitleLight = TextStyle(
     color: subTitleTextColor,
-    fontSize: 2 * SizeConfig.textMultiplier,
-    height: 1.5,
+    fontSize: 2.5 * SizeConfig.textMultiplier,
+    // height: 1.5,
+    fontStyle: FontStyle.italic,
+    fontWeight: FontWeight.w200
   );
 
   static final TextStyle _buttonLight = TextStyle(
@@ -50,7 +54,7 @@ class StyleTheme{
   );
 
   static final TextStyle _searchLight = TextStyle(
-    color: Colors.black,
+    color: Colors.red,
     fontSize: 2.3 * SizeConfig.textMultiplier,
   );
 
