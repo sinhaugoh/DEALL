d584ac6c55c950d70c28cc5db3f1ff91b0408586
Goh Sin Hau <shgoh025@mymail.sim.edu.sg>
2021-08-05 22:10:01 +0800

    implement produt and productDTO

---
+++ lib/core/application/product.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'product.freezed.dart';
+
+@freezed
+class Product with _$Product {
+  const Product._();
+  const factory Product({
+    required String id,
+    required String name,
+    required double usualPrice,
+    required double discountedPrice,
+    required String image,
+    required String description,
+    required bool availability,
+  }) = _Product;
+}
