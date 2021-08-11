ac9274609483fee11611c9b81b2a66c334390f77
Lawrence Ho <lawrencehsj@gmail.com>
2021-08-11 23:05:58 +0800

    solved network image issues

--- lib/consumer/presentation/widgets/custom_card.dart
+++ lib/consumer/presentation/widgets/custom_card.dart
@@ -1,4 +1,5 @@
 import 'package:deall/core/application/retailer.dart';
+import 'package:deall/core/presentation/widgets/images.dart';
 import 'package:flutter/material.dart';
 
 class CustomCard extends StatelessWidget {
@@ -8,6 +9,8 @@ class CustomCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+
     return Card(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
         // color: Colors.grey,
@@ -19,21 +22,59 @@ class CustomCard extends StatelessWidget {
             Expanded(
               flex: 1,
               child: Container(
-                // width: 50,
-                // height: 50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.1),
-                      blurRadius: 20.0,
-                      // spreadRadius: 2.0
-                    ),
-                  ],
-                  image: DecorationImage(
-                    image: retailerData.image.toString() == '' ? NetworkImage("https://www.watchuseek.com/attachments/image-not-found-png.15799454/") : NetworkImage(retailerData.image),
-                    fit: BoxFit.cover,
-                  )
+                width: mediaQuery.size.width,
+                height: mediaQuery.size.height,
+              // retailerData.image.toString() == '' ? 
+                  // FittedBox(
+                  //   clipBehavior: Clip.hardEdge,
+                  //   child: Image.asset(
+                  //     Images.imageNotFound, 
+                  //     fit: BoxFit.cover,
+                  //     // width: MediaQuery.of(context).size.width,
+                  //     // height: MediaQuery.of(context).size.height,
+                  //     scale: 1.1,
+                  //   ),
+                  // ) : Image.network(retailerData.image, fit: BoxFit.cover,
+
+                // width: 350,
+                // height: 340,
+                // borderRadius: BorderRadius.circular(20.0),
+
+                // decoration: BoxDecoration(
+                //   // image: AssetImage('image-not-found.png'),
+                  // borderRadius: BorderRadius.circular(20),
+                //       color: Colors.blue,
+                //   boxShadow: [
+                //     BoxShadow(
+                //       color: Colors.grey.withOpacity(0.1),
+                //       blurRadius: 20.0,
+                      
+                //       // spreadRadius: 2.0
+                //     ),
+                //   ],
+                //   image: DecorationImage(
+                //     image: retailerData.image.toString() == '' ? FileImage(Images.imageNotFound) : NetworkImage(retailerData.image),
+                //     fit: BoxFit.cover,
+                //   )
+                // ),
+
+                // child: FadeInImage(
+                //   width: mediaQuery.size.width,
+                //   height: mediaQuery.size.height,
+                //   image: NetworkImage(retailerData.image), 
+                //   placeholder: AssetImage(Images.logo),
+                //   fit: BoxFit.cover,
+                // ),
+
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20.0),
+                  child: retailerData.image.toString() == '' ? 
+                    Image.asset(
+                      Images.imageNotFound, 
+                      fit: BoxFit.cover,
+                      // width: MediaQuery.of(context).size.width,
+                      // height: MediaQuery.of(context).size.height,
+                    ) : Image.network(retailerData.image, fit: BoxFit.cover,),
                 ),
               ),
             ),
@@ -66,7 +107,7 @@ class CustomCard extends StatelessWidget {
                       )
                     ],
                   ),
-                  trailing: Icon(Icons.add),
+                  // trailing: Icon(Icons.add),
                 )
               )
             )
