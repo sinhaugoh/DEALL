a3afb2d97ae697484d66a586d3a0f4e068dc7704
Kwang Kian Hui <2k1h297@gmail.com>
2021-08-14 19:27:15 +0800

    Implement basic UI for consumer - retailer detail page

--- lib/consumer/presentation/consumer_retailer_detail_page.dart
+++ lib/consumer/presentation/consumer_retailer_detail_page.dart
@@ -3,15 +3,65 @@ import 'package:flutter/material.dart';
 
 class ConsumerRetailerDetailPage extends StatelessWidget {
   final Retailer retailerData;
-  const ConsumerRetailerDetailPage(this.retailerData, { Key? key }) : super(key: key);
+  const ConsumerRetailerDetailPage(this.retailerData, {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final mq = MediaQuery.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text(retailerData.name),
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          const SizedBox(height: 10),
+          SizedBox(
+            height: mq.size.height * 0.4,
+            child: const Placeholder(),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(width: 2),
+              ),
+              child: Text(retailerData.description),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(width: 2),
+              ),
+              child: Column(
+                children: [
+                  const Text("Operating Hours", style: TextStyle(fontWeight: FontWeight.bold)),
+                  Text(retailerData.operatingHours),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(width: 2),
+              ),
+              child: Column(
+                children: [
+                  const Text("Address", style: TextStyle(fontWeight: FontWeight.bold)),
+                  Text(retailerData.addressLine()),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
