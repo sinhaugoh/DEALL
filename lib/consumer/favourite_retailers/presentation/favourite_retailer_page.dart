0952efb4fc2967efb73d616539d48280b7557554
TSCOTech <theshippingcompanyofficial@gmail.com>
2021-09-01 15:15:05 +0800

    test

--- lib/consumer/favourite_retailers/presentation/favourite_retailer_page.dart
+++ lib/consumer/favourite_retailers/presentation/favourite_retailer_page.dart
@@ -1,31 +1,14 @@
-import 'dart:async';
-
-import 'package:connectivity/connectivity.dart';
 import 'package:deall/consumer/presentation/retailer_list_item.dart';
 import 'package:deall/consumer/shared/providers.dart';
 import 'package:deall/core/presentation/widgets/consumer_drawer_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavouriteRetailerPage extends ConsumerStatefulWidget {
+class FavouriteRetailerPage extends ConsumerWidget {
   const FavouriteRetailerPage({Key? key}) : super(key: key);
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() =>
-      _FavouriteRetailerPageState();
-}
-
-class _FavouriteRetailerPageState extends ConsumerState<FavouriteRetailerPage> {
-  StreamSubscription<ConnectivityResult>? _subscription;
-
-  @override
-  void dispose() { 
-    super.dispose();
-    _subscription?.cancel();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Favourite'),
@@ -56,28 +39,15 @@ class _FavouriteRetailerPageState extends ConsumerState<FavouriteRetailerPage> {
                 });
           },
           failure: (firestoreFailure) {
-            return firestoreFailure.maybeWhen(noConnection: () {
-              _subscription = Connectivity()
-                  .onConnectivityChanged
-                  .listen((ConnectivityResult result) async {
-                if (result != ConnectivityResult.none) {
-                  Future.microtask(() async {
-                    ref
-                        .read(favouriteRetailerStateNotifierProvider.notifier)
-                        .getRetailerList();
-                  });
-                  _subscription?.cancel();
-                }
-              });
-
-              return const Center(
-                child: Text('No connection'),
-              );
-            }, orElse: () {
-              return const Center(
-                child: Text('Unexpected error. Please contact support.'),
-              );
-            });
+            return firestoreFailure.maybeWhen(
+                noConnection: () => const Center(
+                      child: Text('No connection'),
+                    ),
+                orElse: () {
+                  return const Center(
+                    child: Text('Unexpected error. Please contact support.'),
+                  );
+                });
           },
         ),
       ),
--- lib/retailer/presentation/retailer_profile_page.dart
+++ lib/retailer/presentation/retailer_profile_page.dart
@@ -1,10 +1,14 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:deall/core/presentation/widgets/images.dart';
+import 'package:deall/core/application/retailer/retailer.dart';
 import 'package:deall/core/presentation/routes/app_router.gr.dart';
 import 'package:deall/core/presentation/widgets/retailer_drawer_widget.dart';
+import 'package:deall/core/presentation/widgets/form_text_field.dart';
 import 'package:deall/retailer/shared/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class RetailerProfilePage extends ConsumerWidget {
   const RetailerProfilePage({Key? key}) : super(key: key);
@@ -20,7 +24,6 @@ class RetailerProfilePage extends ConsumerWidget {
         title: Text(retailer?.name ?? ''),
         actions: [
           IconButton(
-            
             onPressed: retailer == null
                 ? () {}
                 : () {
@@ -40,15 +43,62 @@ class RetailerProfilePage extends ConsumerWidget {
           : SingleChildScrollView(
               child: Column(
                 children: [
-                  if (retailer.image != '')
-                    CachedNetworkImage(
-                      imageUrl: retailer.image,
-                      placeholder: (context, url) =>
-                          const CircularProgressIndicator(),
+                  // if (retailer.image != '')
+                  //   CachedNetworkImage(
+                  //     imageUrl: retailer.image,
+                  //     placeholder: (context, url) =>
+                  //         const CircularProgressIndicator(),
+                  //   ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20.0.w),
+                    child: retailer.image == ''
+                        ? Image.asset(
+                            Images.imageNotFound,
+                            fit: BoxFit.fitHeight,
+                            // width: MediaQuery.of(context).size.width,
+                            // height: MediaQuery.of(context).size.height,
+                          )
+                        : Image.network(
+                            retailer.image,
+                            fit: BoxFit.fitHeight,
+                          ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(20.0.w),
                     ),
-                  Text(retailer.description),
-                  Text(retailer.operatingHours),
-                  Text(retailer.addressLine()),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(retailer.operatingHours),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(20.0.w),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(retailer.addressLine()),
+                    ),
+                  ),
+
+                  // Padding(
+                  //   padding: const EdgeInsets.all(16.0),
+                  //   child: Text(retailer.description),
+                  // ),
+                  // Padding(
+                  //   padding: const EdgeInsets.all(16.0),
+                  //   child: Text(retailer.operatingHours),
+                  // ),
+                  // Padding(
+                  //   padding: const EdgeInsets.all(16.0),
+                  //   child: Text(retailer.addressLine()),
+                  // ),
+                  //Text(retailer.description),
+                  //Text(retailer.operatingHours),
+                  //Text(retailer.addressLine()),
                 ],
               ),
             ),
