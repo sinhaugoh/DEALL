86bb6a4fa8d693190620535589e7303dd6105823
JXBdr <kohjiaxiang15@gmail.com>
2021-09-04 22:36:36 +0800

    UI update
    change to cached image for better performance

--- lib/consumer/presentation/consumer_retailer_detail_page.dart
+++ lib/consumer/presentation/consumer_retailer_detail_page.dart
@@ -2,6 +2,7 @@ import 'package:deall/core/application/retailer/retailer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:deall/core/presentation/widgets/images.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class ConsumerRetailerDetailPage extends StatelessWidget {
   final Retailer retailerData;
@@ -23,39 +24,26 @@ class ConsumerRetailerDetailPage extends StatelessWidget {
           : SingleChildScrollView(
               child: Column(
                 children: [
-                  // if (retailer.image != '')
-                  //   CachedNetworkImage(
-                  //     imageUrl: retailer.image,
-                  //     placeholder: (context, url) =>
-                  //         const CircularProgressIndicator(),
-                  //   ),
                   SizedBox(
                     height: 30.h,
                   ),
-
-                  SizedBox(
-                    height: 200.h,
-                    width: 200.w,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20.0.w),
-                      child: retailerData.image == ''
-                          ? Image.asset(
-                              Images.imageNotFound,
-                              fit: BoxFit.fitHeight,
-                              // width: MediaQuery.of(context).size.width,
-                              // height: MediaQuery.of(context).size.height,
-                            )
-                          : Image.network(
-                              retailerData.image,
-                              fit: BoxFit.fitHeight,
-                            ),
+                  if (retailerData.image != '')
+                    SizedBox(
+                      height: 200.h,
+                      width: 200.w,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0.w),
+                        child: CachedNetworkImage(
+                          imageUrl: retailerData.image,
+                          fit: BoxFit.cover,
+                          placeholder: (context, url) =>
+                              const CircularProgressIndicator(),
+                        ),
+                      ),
                     ),
-                  ),
-
                   SizedBox(
                     height: 16.h,
                   ),
-
                   Container(
                       decoration: BoxDecoration(
                         border: Border.all(color: Colors.grey),
@@ -73,24 +61,23 @@ class ConsumerRetailerDetailPage extends StatelessWidget {
                                     color: Colors.grey.shade700,
                                     fontSize: 16.sp,
                                     fontWeight: FontWeight.bold)),
-                            if (retailerData.description.isEmpty != true)
+                            if (retailerData.description.isNotEmpty)
                               TextSpan(
                                   text: retailerData.description,
                                   style: TextStyle(color: Colors.grey))
                             else
                               const TextSpan(
-                                  text: 'No content has been found',
+                                  text:
+                                      'The retailer has not provided any information yet',
                                   style: TextStyle(
                                       fontStyle: FontStyle.italic,
                                       color: Colors.grey)),
                           ],
                         ),
                       )),
-
                   SizedBox(
                     height: 16.h,
                   ),
-
                   Container(
                       decoration: BoxDecoration(
                         border: Border.all(color: Colors.grey),
@@ -108,24 +95,23 @@ class ConsumerRetailerDetailPage extends StatelessWidget {
                                     color: Colors.grey.shade700,
                                     fontSize: 16.sp,
                                     fontWeight: FontWeight.bold)),
-                            if (retailerData.operatingHours.isEmpty != true)
+                            if (retailerData.operatingHours.isNotEmpty)
                               TextSpan(
                                   text: retailerData.operatingHours,
                                   style: TextStyle(color: Colors.grey))
                             else
                               const TextSpan(
-                                  text: 'No content has been found',
+                                  text:
+                                      'The retailer has not provided any information yet',
                                   style: TextStyle(
                                       fontStyle: FontStyle.italic,
                                       color: Colors.grey)),
                           ],
                         ),
                       )),
-
                   SizedBox(
                     height: 16.h,
                   ),
-
                   Container(
                       decoration: BoxDecoration(
                         border: Border.all(color: Colors.grey),
@@ -143,13 +129,14 @@ class ConsumerRetailerDetailPage extends StatelessWidget {
                                     color: Colors.grey.shade700,
                                     fontSize: 16.sp,
                                     fontWeight: FontWeight.bold)),
-                            if (retailerData.addressLine().isEmpty != true)
+                            if (retailerData.addressLine().isNotEmpty)
                               TextSpan(
                                   text: retailerData.addressLine(),
                                   style: TextStyle(color: Colors.grey))
                             else
                               const TextSpan(
-                                  text: 'No content has been found',
+                                  text:
+                                      'The retailer has not provided any information yet',
                                   style: TextStyle(
                                       fontStyle: FontStyle.italic,
                                       color: Colors.grey)),
