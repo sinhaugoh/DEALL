ec36cf5277bfd13a2a94de0ca289323deb0f86b4
Kwang Kian Hui <2k1h297@gmail.com>
2021-09-18 17:37:25 +0800

    implement new appbar for add product page

--- lib/retailer/product/presentation/add_product_page.dart
+++ lib/retailer/product/presentation/add_product_page.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:deall/core/presentation/routes/app_router.gr.dart';
 import 'package:deall/core/presentation/saving_in_progress_overlay.dart';
@@ -47,7 +48,20 @@ class _AddProductPageState extends ConsumerState<AddProductPage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Add Product'),
+        toolbarHeight: 140.h,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+            bottom: Radius.elliptical(350.w, 50.h),
+          ),
+        ),
+        title: Text('Add Product', style: Theme.of(context).textTheme.headline2),
+        // actions: [
+        //   // iconbtn!,
+        // ],
+        elevation: 0,
+        iconTheme: const IconThemeData(
+          color: Colors.white
+        ),
         centerTitle: true,
       ),
       body: SafeArea(
