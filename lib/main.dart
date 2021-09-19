102e98f1467ec2f9b7695f4806a7a9c8fa4c1d47
sinhaugoh <sinhaugoh456@hotmail.com>
2021-09-19 20:27:56 +0800

    disable landscape mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'core/presentation/app_widget.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   await Firebase.initializeApp();
   runApp(ProviderScope(child: AppWidget()));
 }
